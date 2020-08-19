c5180b47b31aa413ac0c9bf0cabf40bcfaccbed1
Keshavan Srivatsan <ksrivats@usc.edu>
2020-08-18 19:29:01 -0700

    Added new test files

--- anothertest/anothertest/main.cpp
+++ anothertest/anothertest/main.cpp
@@ -1,17 +1,15 @@
 //
 //  main.cpp
-//  test
+//  anothertest
 //
 //  Created by Keshavan Srivatsan on 8/18/20.
 //  Copyright © 2020 Kesha Srivatsan. All rights reserved.
 //
 
 #include <iostream>
-using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int number = 5;
-    cout << 5 << endl;
+    std::cout << "Hello, World!\n";
     return 0;
 }
