744d72533d93a547dd322a69ea346a92cdf4a8b1
Keshavan Srivatsan <ksrivats@usc.edu>
2020-08-19 11:21:14 -0700

    added hello_world

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello_world
+//
+//  Created by Keshavan Srivatsan on 8/19/20.
+//  Copyright © 2020 Kesha Srivatsan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
