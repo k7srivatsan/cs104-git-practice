1f0ba151a82ce1e364757cbaa358d53930c15c50
Keshavan Srivatsan <ksrivats@usc.edu>
2020-08-18 19:10:16 -0700

    created new test file

---
+++ test/test/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Keshavan Srivatsan on 8/18/20.
+//  Copyright © 2020 Kesha Srivatsan. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int number = 5;
+    cout << 5 << endl;
+    return 0;
+}
